2a4f29184fc9a49044054b42999f0a8dadf2f547
sociopath <sociopath164@gmail.com>
2020-02-04 14:40:17 -0500

    Se hacen pruebas de hola mundo con JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/styles.scss";
+import Curso from './curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+    <div className="main-banner img-container" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+      <div className="lg-cols-4 lg-x-2">
+        <img className="main-banner__img" alt="banner" src="https://images.unsplash.com/photo-1580675913888-8a4571c91ee7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80"/>
+        <div className="main-banner__data s-center">
+          <p className="t2 s-mb-0">Título del banner</p>
+          <p> Subtítulo del banner</p>
+          <a href="https://ed.team" className="button">Botón del banner</a>
+        </div>
+      </div>
     </div>
-  );
-}
+  </div>
+
+  <div className="ed-grid m-grid-3">
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+    <Curso />
+  </div>
+  </>
+  
+  )
+
+
 
 export default App;
