abf402e0b749b80d4eb0fec614831c40ed4b79b5
sociopath <sociopath164@gmail.com>
2020-02-04 16:47:00 -0500

    Se manipulan los componentes para mostrar valores dinamicamente, mediante el manejo de arrays

--- src/App.js
+++ src/App.js
@@ -2,6 +2,37 @@ import React from 'react';
 import "./styles/styles.scss";
 import Curso from './curso'
 
+const cursos = [
+  {
+    "key"       : 1,
+    "title"     : "React desde cero",
+    "img"       : "https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+    "price"     : 30,
+    "profesor"  : "Beto Quiroga"
+  
+    },
+  {
+    "key"       : 2,
+    "title"     : "HTML desde cero",
+    "img"       : "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/HTML-2018.jpg?itok=Gyvm-W9t",
+    "price"     : 20,
+    "profesor"  : "Beto Quiroga"
+    },
+  {
+    "key"       : 3,
+    "title"     : "CSS desde cero",
+    "img"       : "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/css-poster_1.jpg?itok=VUK9BKuY",
+    "price"     : 50,
+    "profesor"  : "Beto Quiroga"
+    },
+  {
+    "key"       : 4,
+    "title"     : "JS desde cero",
+    "img"       : "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/javascript_0.jpg?itok=aKHihrP0",
+    "price"     : 40,
+    "profesor"  : "Beto Quiroga"
+    }]
+
 const App = () => (
   <>
     <div className="main-banner img-container" id="main-banner">
@@ -17,13 +48,18 @@ const App = () => (
     </div>
   </div>
 
-  <div className="ed-grid m-grid-3">
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
+  <div className="ed-grid m-grid-4">
+   {
+     cursos.map( curso => <Curso 
+                          key = {curso.key}
+                          title = {curso.title}
+                          img = {curso.img}
+                          price = {curso.price}
+                          profesor = {curso.profesor}
+                           />)
+   }
+    
+   
   </div>
   </>
   
--- src/curso.jsx
+++ src/curso.jsx
@@ -1,31 +1,49 @@
 import React from 'react'
-
-const Curso = () =>(
+import propTypes from 'prop-types'
+const Curso = ({key,title, img, profesor, imgProfesor, price}) =>(
     
-    <article className="card l-container">
+    <article className="card l-container s-pt-4" key={key}>
     <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-      <img src="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2020-01/ui_design.png" alt=""/>
+      <img src={img} alt={title}/>
     </div>
     <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
       <h3 className="t5 s-mb-2 s-center">
-        Programación orientada a objetos con Go
+         {title}
       </h3>
       <div className="s-mb-2 s-main-center">
         <div className="card__teacher s-cross-center">
           <div className="card__avatar s-mr-1">
             <div className="circle img-container">
-              <img src="https://api.ed.team/files/avatars/18a0441b-8357-488f-a461-21d07260f46a.jpg" alt=""/>
+              <img src={imgProfesor} alt="" />
             </div>
           </div>
-          <span className="small">Alexys Lozada</span>
+          <span className="small">{profesor}</span>
         </div>
       </div>
       <div className="s-main-center">
-        <a className="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+        <a className="button--ghost-alert button--tiny" href="https://ed.team">{price}</a>
       </div>
     </div>
   </article>
   
   )
+
+  Curso.propTypes ={
+    title : propTypes.string,
+    img: propTypes.string,
+    price: propTypes.string,
+    profesor: propTypes.string,
+    
+  }
+
+  Curso.defaultProps = {
+    title : "No se encontró titulo",
+    img : "https://stockpictures.io/wp-content/uploads/2020/01/image-not-found-big-768x432.png",
+    profesor : "",
+    imgProfesor: "",
+    price : "----"
+
+
+  }
 
 export default Curso
